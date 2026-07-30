41b572a3c793a21b1bcd3ccce46ac528db7272ec
Alejandro <ramirezaed@gmail.com>
2026-07-30 01:07:50 -0300

    feat: agrega funciones para verificar stock, descontar stock, y calcular subtotal

--- src/Models/products/Producto.php
+++ src/Models/products/Producto.php
@@ -45,4 +45,34 @@ class Product
     {
         return $this->category;
     }
+
+    // funcion para verificar si el stock disponible del producto es suficiente para la cantidad solicitada
+    // devuelve true o false
+    public function sufficientStock(int $amount): bool
+    {
+        //verifica que cantidad sea nuemero positivo
+        if ($amount <= 0) {
+            return false;
+        }
+        //si stock es menor que la cantidad solicitada , devuelve false
+        return $this->stock >= $amount;
+    }
+
+    public function deductStock(int $amount): void
+    {
+        //llama a la funcion para verificar si tiene stock suficiente
+        if ($this->sufficientStock($amount)) {
+            // -=  resta el valor de una variable, y asigna en ella el resultado
+            $this->stock -= $amount;
+        }
+    }
+
+    public function calculateSubtotal(int $amount): float
+    {
+        //verifica que la cantidad sea positiva
+        if ($amount <= 0) {
+            return false;
+        }
+        return $this->price * $amount;
+    }
 }
